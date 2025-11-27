29fa2e31b183936ba86686f1df69f77d5d22207e
Kim Dong Young <lovecom12340@naver.com>
2025-11-27 05:13:52 +0000

    B

--- Test.java
+++ Test.java
@@ -1,5 +1,6 @@
 class Test{
     public static void main(String[] args){
         System.out.println("Hello! Java and git!");
+        System.out.println("지금 나는 merge와 branch에 대해 연습중이다");
     }   
 }
